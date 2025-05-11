1a307994811b6505a68a17f2e6ec1d70f3fe0da8
Lariterrinha <lalaterrinha14@gmail.com>
2025-05-10 21:16:11 -0300

    Pagina de configurações gerais

--- checkpointapp/lib/main.dart
+++ checkpointapp/lib/main.dart
@@ -1,20 +1,24 @@
 import 'package:checkpointapp/root.dart';
 import 'package:checkpointapp/sobre_o_app.dart';
 import 'package:flutter/material.dart';
-// Remova a importação duplicada: import 'root.dart';
+import 'BancoDeDados/UserPreferencesServices.dart';
 import 'login/login_page.dart';
 import 'login/startpage.dart';
 import 'login/signup.page.dart';
 import 'login/reset-password.page.dart';
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_auth/firebase_auth.dart'; // Importe o pacote de autenticação
+import 'package:firebase_auth/firebase_auth.dart';
 import 'firebase_options.dart';
 
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   try {
+
+    // Tema e configurações iniciais
+    await UserPreferencesService.loadPreferences();
+
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
@@ -35,17 +39,16 @@ class MyApp extends StatelessWidget {
       title: 'Checkpoint App',
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       debugShowCheckedModeBanner: false,
-       //a navegação inicial será tratada pelo 'home'.
+
+      // Rotas
       routes: {
         '/start': (context) => StartPage(),
         '/login': (context) => LoginPage(),
         '/cadastro': (context) => SignupPage(),
         '/reset-password': (context) => ResetPasswordPage(),
-        '/tela_principal': (context) => RootPage(), // Esta será sua tela principal após o login
+        '/tela_principal': (context) => RootPage(),
         '/sobre_o_app': (context) => SobreoApp(),
       },
-
-
 
 
       // O 'home' agora verifica o estado de autenticação(se ta logado ou não)
